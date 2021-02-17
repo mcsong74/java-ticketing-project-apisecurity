603f0c57683d6c026dcee7e94aec9157b13a4b68
ssonata74 <ssonata@gmail.com>
2021-02-16 19:19:07 -0500

    Java Ticketing project - REST - api security - email confirmation

--- src/main/java/com/cybertek/repository/ConfirmationTokenRepository.java
+++ src/main/java/com/cybertek/repository/ConfirmationTokenRepository.java
@@ -4,7 +4,10 @@ import com.cybertek.entity.ConfirmationToken;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-public interface ConfirmationTokenRepository extends JpaRepository<ConfirmationToken, Integer> {
+public interface ConfirmationTokenRepository extends JpaRepository<ConfirmationToken, Long> {
+    Optional<ConfirmationToken> findByToken(String token);
 
 }
---
+++ src/main/java/com/cybertek/service/ConfirmationTokenService.java
@@ -0,0 +1,18 @@
+package com.cybertek.service;
+
+import com.cybertek.entity.ConfirmationToken;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.stereotype.Service;
+
+@Service
+public interface ConfirmationTokenService {
+    //need ConfirmationToken object returned to send email to the user
+    ConfirmationToken save (ConfirmationToken confirmationToken);
+
+    void sendEmail(SimpleMailMessage email);
+
+    ConfirmationToken readByToken(String token);
+
+
+
+}
